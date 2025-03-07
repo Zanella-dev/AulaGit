25f02220ee51b50d56f485277568296b331043a6
Zanella-dev <krzanela@minha.fag.edu.br>
2025-03-07 20:35:03 -0300

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package aula_01;
+
+public class Hello_Java {
+
+	public Hello_Java() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Word");
+	}
+
+}
